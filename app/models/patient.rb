d653475c61147967dcffbca01ebd7b1e803d3666
Hammad <aw03495214053@gmail.com>
2022-02-04 18:49:23 +0500

    PD_3_Patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,6 @@
 class Patient < User
   validates_presence_of :first_name, :last_name, :email, :phone_number
-  validates :phone_number, uniqueness: true
-  validates_length_of :first_name, :last_name, :phone_number, length: { is: 11 }
+  validates :phone_number, uniqueness: true, length: { is: 11 }
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, uniqueness: true
 
   def self.columns
