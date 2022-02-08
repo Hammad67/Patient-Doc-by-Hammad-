ef7d4d2e085e2859684ffd4eba497efc7e685496
Hammad <aw03495214053@gmail.com>
2022-02-08 20:02:10 +0500

    PD-5-visit-module

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,4 +1,6 @@
 class VisitsController < ApplicationController
+  before_action :get_appointment_and_patient
+
   def new
     @appointment = Appointment.find(params[:appointment_id])
     @patient = Patient.find(params[:patient_id])
@@ -9,5 +11,17 @@ class VisitsController < ApplicationController
       redirect_to patient_appointment_visit_path(@patient, @appointment, @visit)
       flash[:notice] = "Patient has successfully visit"
     end
+  end
+
+  def show
+    @visit = Visit.find(params[:id])
+
+  end
+
+  private
+
+  def get_appointment_and_patient
+    @appointment = Appointment.find(params[:appointment_id])
+    @patient = Patient.find(params[:patient_id])
   end
 end
