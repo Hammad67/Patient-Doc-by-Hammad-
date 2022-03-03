c2da8cd7fdf54e87b4f6b03b7bfba48ffe7b99ef
Hammad677 <hammad.rashid@piecyfer.com>
2022-03-03 17:21:26 +0500

    conventions followed

--- app/interactors/create_medical_record.rb
+++ app/interactors/create_medical_record.rb
@@ -2,9 +2,9 @@ class CreateMedicalRecord
   include Interactor
 
   def call
-    medical = MedicalRecord.new(context.medical_params)
-    medical.patient_id = context.patient_id
-    context.medical = medical
-    context.fail! unless medical.save
+    medical_record = MedicalRecord.new(context.medical_params)
+    medical_record.patient_id = context.patient_id
+    context.medical_record = medical_record
+    context.fail! unless medical_record.save
   end
 end
